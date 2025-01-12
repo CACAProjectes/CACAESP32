8f967c4c0dcf73b8abe9a1fce70187285decf259
CACAProjects <cacaprojectsapps@gmail.com>
2025-01-12 09:30:49 +0100

    Renovar carpeta

--- 01_Connect_WiFi_AP/config.h
+++
@@ -1,7 +0,0 @@
-const char* ssid     = "ESP32_CACA_SSID";
-const char* password = "12345";
-const char* hostname = "ESP32_CACA";
-
-IPAddress ip(192, 168, 1, 226);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
--- ESP32-Examples-main/06 - Comunicación WiFi/03_MDNS/ESP32_Utils_mDNS.hpp
+++
@@ -1,8 +0,0 @@
-void InitMDNS()
-{
-   if (!MDNS.begin(hostname)) 
-   {             
-     Serial.println("Error iniciando mDNS");
-   }
-   Serial.println("mDNS iniciado");
-}
