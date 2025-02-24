53898d357511f44d1a0ebca196e110f4b9fb1770
Juanje <campospulido@hotmail.com>
2025-02-24 12:05:51 +0100

    Página Web

--- WiFiAccessPoint/WiFiAccessPoint.ino
+++ WiFiAccessPoint/WiFiAccessPoint.ino
@@ -15,16 +15,18 @@
 #include <NetworkClient.h>
 #include <WiFiAP.h>
 #include "esp_wifi.h"
-#include "pagina_web.h"
 
-//#ifndef LED_BUILTIN
-//#define LED_BUILTIN 2  // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
-//#endif
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 4  // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+#endif
 
 // Set these to your desired credentials.
 const char *ssid = "CACA_ESP32_C3";
 const char *password = "12345678";
-int LED_BUILTIN = 4;
+//int LED_BUILTIN = 4;
+
+// Set VAR PAGINA HTML
+String pagina_web;
 
 NetworkServer server(80);
 
@@ -78,6 +80,14 @@ void loop() {
             // the content of the HTTP response follows the header:
             //client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
             //client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
+            pagina_web = getPaginaWeb();
+            //
+            pagina_web.replace("{0}", "11,3");        // Sensor proximidad DELANTERO
+            pagina_web.replace("{1}", "8,5");         // Sensor proximidad TRASERO
+            pagina_web.replace("{2}", "ENCENDIDA");   // Marcha atrás
+            pagina_web.replace("{3}", "APAGADA");     // Freno-STOP
+            pagina_web.replace("{4}", "86");          // Unidad de potencia
+            //
             client.print(pagina_web);
 
             // The HTTP response ends with another blank line:
@@ -107,17 +117,63 @@ void loop() {
 }
 
 // Set VAR PAGINA HTML
-/*
-const char *pagina_web = "<!doctype html>";
-*pagina_web += "<html lang=\"es\">";
-*pagina_web += "<head>";
-*pagina_web += "<title>P R I N C I P A L</title>";
-*pagina_web += "<meta charset=\"UTF-8\">";
-*pagina_web += "<meta http-equiv=\"refresh\" content=\"100; url=./K\">";
-*pagina_web += "</head>";
-*pagina_web += "<html>";
-*pagina_web += "<body>";
-*pagina_web += "Hi gays";
-*pagina_web += "</body>";
-*pagina_web += "</html>";
-*/
+String getPaginaWeb() {
+  String strPaginaWeb = String("<!doctype html>");
+  strPaginaWeb += String("<html lang=\"es\">");
+  strPaginaWeb += String("<head>");
+  strPaginaWeb += String("<title>CACA_ESP32_C3 - C a r</title>");
+  strPaginaWeb += String("<meta charset=\"UTF-8\">");
+  strPaginaWeb += String("<meta http-equiv=\"refresh\" content=\"100; url=./K\">");
+  strPaginaWeb += String("</head>");
+  strPaginaWeb += String("<html>");
+  strPaginaWeb += String("<body>");
+  
+  strPaginaWeb += String("<table border=0>");
+  strPaginaWeb += String("<tr><td>LUCES POSICI&Oacute;N</a></td><td><a href=\"./LPO=S\">ENCENDER</a> / <a href=\"./LPO=N\">APAGAR</a></td></tr>");
+  strPaginaWeb += String("<tr><td>LUCES CRUCE</a></td><td><a href=\"./LCR=S\">ENCENDER</a> / <a href=\"./LCR=N\">APAGAR</a></td></tr>");
+  strPaginaWeb += String("<tr><td>LUCES CARRETERA</a></td><td><a href=\"./LCA=S\">ENCENDER</a> / <a href=\"./LCA=N\">APAGAR</a></td></tr>");
+  strPaginaWeb += String("<tr><td>WARNINGS</a></td><td><a href=\"./WAR=S\">ENCENDER</a> / <a href=\"./WAR=N\">APAGAR</a></td></tr>");
+  strPaginaWeb += String("<tr><td>SIRENA</a></td><td><a href=\"./SIR=S\">ENCENDER</a> / <a href=\"./SIR=N\">APAGAR</a></td></tr>");
+  strPaginaWeb += String("<tr><td colspan=2>&nbsp;</td></tr>");
+  strPaginaWeb += String("<tr><td>Sensor proximidad DELANTERO</td><td>{0}&percnt;</td></tr>");
+  strPaginaWeb += String("<tr><td>Sensor proximidad TRASERO</td><td>{1}&percnt;</td></tr>");
+  strPaginaWeb += String("<tr><td>Marcha atrás</td><td>{2}</td></tr>");
+  strPaginaWeb += String("<tr><td>Freno-STOP</td><td>{3}</td></tr>");
+  strPaginaWeb += String("<tr><td colspan=2>&nbsp;</td></tr>");
+  strPaginaWeb += String("<tr><td>Unidad de potencia</td><td>{4}&percnt;</td></tr>");
+  strPaginaWeb += String("<tr>");
+	strPaginaWeb += String("<td  colspan=2 align=\"center\">");
+	strPaginaWeb += String("&lsqb;<a href=\"./UPO=00\">PARAR</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=10\">10&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=20\">20&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=30\">30&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=40\">40&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=50\">50&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=60\">60&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=70\">70&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=80\">80&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=90\">90&percnt;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./UPO=99\">MAX VELOCIDAD</a>&rsqb;");
+  strPaginaWeb += String("</td>");
+  strPaginaWeb += String("</tr>");
+  strPaginaWeb += String("<tr><td colspan=2>&nbsp;</td></tr>");
+  strPaginaWeb += String("<tr><td>Giro ruedas delanteras</td><td>45&deg;</td></tr>");
+  strPaginaWeb += String("<tr>");
+	strPaginaWeb += String("<td  colspan=2 align=\"center\">");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=10\">-40&deg;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=20\">-30&deg;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=30\">-20&deg;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=40\">-10&deg;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=50\">RECTO</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=60\">+10&deg;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=70\">+20&deg;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=80\">+30&deg;</a>&rsqb;");
+  strPaginaWeb += String("&lsqb;<a href=\"./GRU=90\">+40&deg;</a>&rsqb;");
+  strPaginaWeb += String("</td>");
+  strPaginaWeb += String("</tr>");
+  strPaginaWeb += String("</table>");
+ 
+  strPaginaWeb += String("</body>");
+  strPaginaWeb += String("</html>");
+  return strPaginaWeb;
+}
